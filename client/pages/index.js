787224b292206088d1fbf59caef516980e804c39
Created unity Project <nrhymer@students.kennesaw.edu>
2022-05-01 23:35:45 -0400

    Creates a centered page with responsive display

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,17 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Sidebar from '../components/Sidebar'
 
 const style = {
     wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`, 
-    content: `max-w-[1400px] w-2/3 flex justify-between`
+    content: `max-w-[1400px] w-2/3 flex justify-between` //Creates a centered page with responsive display
 }
 
-const Home: NextPage = () => {
+export default function Home(){
   return (
     <div className={style.wrapper}>
       <div className={style.content}>
-        <h2>Sidebar</h2>
+        <Sidebar/>
         <h2>Feed</h2>
         <h2>Widget</h2>
       </div>
@@ -19,4 +19,3 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
