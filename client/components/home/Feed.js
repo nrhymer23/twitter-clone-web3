fc63cc37796395f73aab54ae719dc85b22b55162
Created unity Project <nrhymer@students.kennesaw.edu>
2022-05-16 23:21:36 -0400

    Working on Post Compoonent stlying complete

--- client/components/home/Feed.js
+++ client/components/home/Feed.js
@@ -1,5 +1,7 @@
 import { BsStar, BsStars } from "react-icons/bs"
+import { FaSlideshare } from "react-icons/fa"
 import TweetBox from "./TweetBox"
+import Post from "../Post"
 
 
 const style = {
@@ -7,6 +9,46 @@ const style = {
     header: `sticky top-0 bg-[#15202b] z-10 p-4 flex justify-between items-center`,
     headerTitle: `text-xl font-bold`,
 }
+
+const tweets = [
+    {
+        displayName: 'No Ls',
+        userName: '',
+        avatar:'',
+        text: 'gm',
+        isProfileImageNft: false,
+        timestamp: '2020-05-17T12:00:00.00Z', // this is how sanity stores timestamps
+
+    },
+    {
+        displayName: 'No Ls',
+        userName: '',
+        avatar:'',
+        text: 'gm',
+        isProfileImageNft: false,
+        timestamp: '2020-05-17T12:00:00.00Z', // this is how sanity stores timestamps
+
+    },
+    {
+        displayName: 'No Ls',
+        userName: '',
+        avatar:'',
+        text: 'gm',
+        isProfileImageNft: false,
+        timestamp: '2020-05-17T12:00:00.00Z', // this is how sanity stores timestamps
+
+    },
+    {
+        displayName: 'No Ls',
+        userName: '',
+        avatar:'',
+        text: 'gm',
+        isProfileImageNft: false,
+        timestamp: '2020-05-17T12:00:00.00Z', // this is how sanity stores timestamps
+
+    },
+
+]
 
 function Feed (){
     return (
@@ -16,6 +58,18 @@ function Feed (){
                 <BsStars />
             </div>
                 <TweetBox />
+                {
+                    tweets.map((tweet, index) => (
+                        <Post 
+                            key={index}
+                            displayName={tweet.displayName}
+                            userName={tweet.avatar}
+                            text={tweet.text}
+                            isProfileImageNft={tweet.isProfileImageNft}
+                            timestamp={tweet.timestamp}
+                            />
+                    ))
+                }
         </div>
     )
 }
